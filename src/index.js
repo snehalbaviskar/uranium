958950d3fa531e8ada92139e02b5c847bc0850e0
snehal <snehalbaviskar2311.com>
2022-04-20 17:22:25 +0530

    assignment 5

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +14,25 @@ mongoose.connect("mongodb+srv://functionUpUranium-2:JECVxS0v96bKoG0a@cluster0.j1
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
+
+
+app.use(function(req,res,next){
+    let date = moment().format('YYYY-MM-DD HH:MM:SS')
+    console.log(`${date}, ${req.socket.remoteAddress}, ${req.path}`)
+    next()
+})
+
+
+/*const moment = require('moment');
+let today = moment();
+//console.log(today.format());
+app.use (function(req,res,next) { console.log(today.format("YYYY-MM-DD"));
+ next()
+})*/
+
+
+
 
 app.use('/', route);
 
