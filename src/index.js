f94b0f833db3816fc2b7575164871481cf46612e
snehal <snehalbaviskar2311.com>
2022-04-23 11:57:59 +0530

    assignment 21 april

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionUpUranium-2:JECVxS0v96bKoG0a@cluster0.j1yrl.mongodb.net/snehal-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
