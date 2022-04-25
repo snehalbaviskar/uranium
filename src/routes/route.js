aa1294f437e918825d52b2da047e79d79c52c912
snehal <snehalbaviskar2311.com>
2022-04-25 17:52:04 +0530

    assignment 23 april

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,6 @@ router.post("/login", userController.loginUser)
 router.get("/users/:userId",mid1, userController.getUserData)
 
 router.put("/users/:userId",mid1, userController.updateUser)
-router.delete("/users/:userId",mid1, userController.deleteUser)
+router.delete("/users/:userId", mid1, userController.deleteUser)
 
 module.exports = router;
