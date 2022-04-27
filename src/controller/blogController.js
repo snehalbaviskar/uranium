b4094cad7fa046e2d7ec2df626151e2d4acff94a
Rubi-chauhan <rubichauhan39@gmail.com>
2022-04-27 22:07:26 +0530

    4apis done

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,14 +1,83 @@
-
+const { default: mongoose } = require('mongoose');
 const blogModel = require('../models/blogModel');
 
+const isValidObjectId =function(Id){
+    return mongoose.Types.ObjectId.isValid(Id)
+}
+const isValid = function(value)
+{
+    if(typeof value === 'undefined' || value === null){
+        return false
+    }
+    if(typeof value === 'string' && value.trim().length == 0){
+        return false
+    }
+    return true
+
+}
+// "       "
+
+
+
+
+
 const createBlog = async function(req,res){
-    let Blog = req.body
+    let data = req.body
+    if(!isValid(data))
+    return res.status(404).send({status:false, msg:"author Id is not exist"})
+
     let authorId = req.body.authorId
-    if(!authorId)
-    res.status(400).send({status:false, msg:"author Id is not exist"})
-    let blogCreated = await blogModel.create(Blog)
+    if(!isValidObjectId(authorId))
+    return res.status(400).send({status:false, msg:"author Id is not exist"})
+    let blogCreated = await blogModel.create(data)
     res.status(201).send({data : blogCreated})
     
 }
 
+const getblog = async function(req,res){
+   
+    let data = req.query
+    const {authorId , category, subcategory, tags} =data
+    let filter = {isDeleted: false, isPublished:true}
+    if(authorId){
+        filter["authorId"]=authorId
+    }
+    if(category){
+        filter["category"]=category
+    }
+    if(subcategory){
+        filter["subcategory"]=subcategory
+    }
+    if(tags){
+        filter["tags"]=tags
+    }
+    
+    let blogs = await blogModel.find(filter)
+    
+    if(blogs.length==0)
+    res.status(404).send({status:false, msg:"document not found"})
+    res.status(200).send({status:true, data: blogs})
+
+}
+
+const updateblog =async function(req,res){
+    let data = req.body
+    let blogId = req.param.blogId
+    let blog = await blogModel.findById(blogId)
+    console.log(blog)
+    if(!!isValidObjectId(blogId))
+    return res.status(404).send({status:false, msg: "blogId is not present"})
+    else{
+    let updatedBlog = await blogModel.findByIdAndUpdate({_Id:blogId}, {$set:data},{new:true, publishedAt:Date.now()}) 
+    req.status(200).send({status:true, data:updatedBlog})  
+    }
+}
+
+
+
+
+
+module.exports.getblog = getblog
+
 module.exports.createBlog = createBlog
+module.exports.updateblog =updateblog
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,12 @@ const BlogController = require('../controller/blogController')
 
 router.post('/createAuthor', AuthorController.createAuthor)
 router.post('/createBlog', BlogController.createBlog)
+router.get('/getblog',BlogController.getblog)
+router.put('/updateblog', BlogController.updateblog)
+
+
+
+// router.get('/filterblog', BlogController.filterblog)
 
 
 
