18b22fcdcfa2abd2f1b955f41acd30decceaaea1
Amit <amit261199@gmail.com>
2022-05-09 16:07:33 +0530

    userModel done

---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema( {
+    title: {type: String, required: true, enum: ["Mr", "Mrs", "Miss"]},
+    name: {type: String, required: true},
+    phone: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, minlength: 8, maxlength: 15},
+    address: {
+        street: {type: String},
+        city: {type: String},
+        pincode: {type: String}
+},   
+}, { timestamps: true });
+
+module.exports = mongoose.model('Users', userSchema)
+
+// { 
+//     title: {string, mandatory, enum[Mr, Mrs, Miss]},
+//     name: {string, mandatory},
+//     phone: {string, mandatory, unique},
+//     email: {string, mandatory, valid email, unique}, 
+//     password: {string, mandatory, minLen 8, maxLen 15},
+//     address: {
+//       street: {string},
+//       city: {string},
+//       pincode: {string}
+//     },
+//     createdAt: {timestamp},
+//     updatedAt: {timestamp}
+//   }
